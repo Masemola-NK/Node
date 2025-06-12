624256516e7b01cc958344fbb1ecd103f63e159d
Masemola-NK <fadeaway445@gmail.com>
2025-06-12 11:13:06 +0200

    Importing files

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
-const fs = require ('fs')
+const getNotes = require('./notes.js')
 
+const hmm =getNotes()
 
-/*Below is to create a file and the adds information*/
-fs.writeFileSync('note.txt','You have been got, Focus up JS is case sensitive buddy!!!')
-console.log('Note has been UPDATED')
+console.log(hmm)
 
-//Adding information to an existing file within a try catch for risk mitigation incase the file doesn't exist
-try{
-fs.appendFileSync('note.txt', '\n\nInformation appended to an exiting file')
-console.log('\n\nThe information was added succesfully')
-}catch(err){
-    console.log('Well looks to be that the file you want to add information to does not exist')
-}
+// const add = require('./notes.js')
+// const sum =add(99,-2)
+// console.log(sum)
---
+++ notes.js
@@ -0,0 +1,15 @@
+console.log('notes.js')
+
+const getNotes =function(){
+        const b ='Your notes....'
+        return (b)
+} 
+
+
+
+
+// const name = 'At-last'
+// const add =function(p,b) {
+//     return(p+b)
+// }
+module.exports= getNotes
