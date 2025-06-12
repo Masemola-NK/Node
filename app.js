9ab03aa08a6f1617fab49aba4ec2a5318230ca69
Masemola-NK <fadeaway445@gmail.com>
2025-06-12 10:34:47 +0200

    nodeJS~FileSystem&error checking

--- app.js
+++ app.js
@@ -5,5 +5,10 @@ const fs = require ('fs')
 fs.writeFileSync('note.txt','You have been got, Focus up JS is case sensitive buddy!!!')
 console.log('Note has been UPDATED')
 
-//Adding information to an existing file
+//Adding information to an existing file within a try catch for risk mitigation incase the file doesn't exist
+try{
 fs.appendFileSync('note.txt', '\n\nInformation appended to an exiting file')
+console.log('\n\nThe information was added succesfully')
+}catch(err){
+    console.log('Well looks to be that the file you want to add information to does not exist')
+}
