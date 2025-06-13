a5ef317532117a4baa58ce8400a5da43b1fa21aa
Masemola-NK <fadeaway445@gmail.com>
2025-06-13 17:30:55 +0200

    Importing npm modules

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+const validator =require('validator')
 const getNotes = require('./notes.js')
 
-const hmm =getNotes()
 
+const hmm =getNotes()
 console.log(hmm)
+
+console.log(validator.isEmail('diamond789@example.com'))
+console.log(validator.isURL('dhbkjbdefw98755'))
 
 // const add = require('./notes.js')
 // const sum =add(99,-2)
