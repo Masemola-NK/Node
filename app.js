03a5725189479333e11987c5550f8e674d48a071
Masemola-NK <fadeaway445@gmail.com>
2025-06-23 13:06:25 +0200

    Argument Passing->CMD

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ const validator =require('validator')
 const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
-
-const hmm =getNotes()
-console.log(hmm)
-
-const msg = chalk.bold.green('LEVITATE...')
-console.log(msg)
+const Msg =getNotes()
+console.log(chalk.strikethrough.bgMagenta(Msg))
 
 console.log(validator.isEmail('diamond789@example.com'))
-console.log(validator.isURL('dhbkjbdefw98755 :  ') + chalk.rgb(255,215,0).bold('\n dhbkjbdefw98755'))
+console.log(validator.isURL('dhbkjbdefw98755git') + chalk.rgb(255,180,0).bold('\n dhbkjbdefw98755'))
 
-// const add = require('./notes.js')
-// const sum =add(99,-2)
-// console.log(sum)
+//Process.Argv is the array created not only so but also indicate the path of the executable...
+//Indicates the the directory where the exe is and also appends a value to the array when script is ran 
+// Therefore the last appended array may be used as the command within a conditional statement
+console.log(process.argv[2])
+
+const id =process.argv[2]
+if (id === 'add' ){
+    console.log(chalk.green.bold.underline('\nAdding Note'))
+}else if (id === 'remove'){
+    console.log(chalk.red.italic.underline('\nRemoving Note'))
+}
+/*Now run command with the extension adding a note to the argv with string {node app.js add --tiltle='I iam the greatest'} */
+console.log(process.argv)
